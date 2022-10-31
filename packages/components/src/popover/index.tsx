a2828ceeed076b48685cb5dcbdac49881200d490
Daniel Richards <daniel.richards@automattic.com>
2022-10-31 11:53:45 +0800

    Allow popover to render inline by overriding slot

--- packages/components/src/popover/index.tsx
+++ packages/components/src/popover/index.tsx
@@ -71,7 +71,7 @@ import { limitShift as customLimitShift } from './limit-shift';
  *
  * @type {string}
  */
-const SLOT_NAME = 'Popover';
+const DEFAULT_SLOT_NAME = 'Popover';
 
 // An SVG displaying a triangle facing down, filled with a solid
 // color and bordered in such a way to create an arrow-like effect.
@@ -176,7 +176,7 @@ const UnforwardedPopover = (
 		anchor,
 		expandOnMobile,
 		onFocusOutside,
-		__unstableSlotName = SLOT_NAME,
+		__unstableSlotName,
 		flip = true,
 		resize = true,
 		shift = false,
@@ -321,7 +321,9 @@ const UnforwardedPopover = (
 	].filter(
 		( m: Middleware | undefined ): m is Middleware => m !== undefined
 	);
-	const slotName = useContext( slotNameContext ) || __unstableSlotName;
+
+	const contextSlotName = useContext( slotNameContext );
+	const slotName = __unstableSlotName ?? contextSlotName ?? DEFAULT_SLOT_NAME;
 	const slot = useSlot( slotName );
 
 	let onDialogClose;
@@ -585,7 +587,7 @@ const UnforwardedPopover = (
 export const Popover = forwardRef( UnforwardedPopover );
 
 function PopoverSlot(
-	{ name = SLOT_NAME }: { name?: string },
+	{ name = DEFAULT_SLOT_NAME }: { name?: string },
 	ref: ForwardedRef< any >
 ) {
 	return (
